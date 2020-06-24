a3ba8fb428b4d826900be8d2fecd25eb03a08166
Campari229 <KennyKoin@gmail.com>
2020-06-24 16:32:35 +0300

    Updates

---
+++ precache-manifest.267d8935ec02a22599bc969c4057f417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c950de7a9b9e110c99379b43d6e9ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d210dc0114b5989bd6e",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "97332077fe707f35d4d8",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "4d210dc0114b5989bd6e",
+    "url": "./static/js/2.23771640.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.23771640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97332077fe707f35d4d8",
+    "url": "./static/js/main.551f2133.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.267d8935ec02a22599bc969c4057f417.js"
 );
 
 self.addEventListener('message', (event) => {
